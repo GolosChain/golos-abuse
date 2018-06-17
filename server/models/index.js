a82556e5a4c5c75b1372ed1a8d31bcfce9ac49e9
rusov.d.s <rusov.d.s@gmail.com>
2018-06-17 11:35:50 +0700

    add  belongsTo relation Complaint -> post

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const User = (module.exports.User = require('./libs/User'))
 const Post = (module.exports.Post = require('./libs/Post'))
 const Complaint = (module.exports.Complaint = require('./libs/Complaint'))
+
+Complaint.belongsTo(Post, { foreignKey: 'postId', as: 'post' })
